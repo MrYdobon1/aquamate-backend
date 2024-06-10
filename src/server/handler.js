f722f8409a80faea3c35c26f24f3616fc2554b0e
Quarantino <60208286+MrYdobon1@users.noreply.github.com>
2024-06-10 07:44:20 +0700

    Add files via upload

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,6 +1,34 @@
+const predictClassification = require('../services/inferenceServices');
+const crypto = require('crypto');
+ 
 async function postPredictHandler(request, h) {
-    /*
-    * Akan diisi setelah membuat logika inferensi machine learning.
-    */
+  const { image } = request.payload;
+  const { model } = request.server.app;
+ 
+  const {label, explanation} = await predictClassification(model, image);
+  const id = crypto.randomUUID();
+
+  if (!label || !explanation) {
+    return h.response({
+      status: 'fail',
+      message: 'Result or explanation is missing'
+    }).code(400);
+  }
+ 
+  const data = {
+    "id": id,
+    "result": label,
+    "explanation": explanation,
+
+  }
+ 
+  const response = h.response({
+    status: 'success',
+    data
+  })
+
+  response.code(201);
+  return response;
 }
+ 
 module.exports = postPredictHandler;
--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,13 @@
 const tf = require('@tensorflow/tfjs-node');
 async function loadModel() {
-    // return tf.loadGraphModel(process.env.MODEL_URL);
+    const modelUrl = 'https://storage.googleapis.com/aquamate-backend/model.json';
+    try {
+        const model = await tf.loadLayersModel(modelUrl);
+        console.log('Sukses'); // Pesan "sukses" akan dicetak ke konsol jika model berhasil dimuat
+        return model;
+    } catch (error) {
+        console.error('Gagal memuat model:', error); // Cetak pesan kesalahan ke konsol jika terjadi kesalahan saat memuat model
+        throw error; // Teruskan kembali kesalahan untuk ditangani di tempat lain jika perlu
+    }
 }
 module.exports = loadModel;
